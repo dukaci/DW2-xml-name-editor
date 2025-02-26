d7992a57bb33d8fd5797c9e1a2a6d57c9adc98eb
Dande <dande.kutka@gmail.com>
2025-02-26 03:44:27 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace DW2_xml_name_editor
+{
+   partial class Form1
+   {
+      private System.ComponentModel.IContainer components = null;
+      private System.Windows.Forms.Button buttonSelectXmlFile;
+      private System.Windows.Forms.TextBox textBoxLogger; // Add a TextBox for logging
+      private System.Windows.Forms.ListBox listBoxXmlElements; // Add a ListBox for XML elements
+      private System.Windows.Forms.TextBox textBoxArrayObjectIdElementName; // Add a TextBox for Array object ID element name
+      private System.Windows.Forms.TextBox textBoxArrayObjectNameElementName; // Add a TextBox for Array object Name element name
+      private System.Windows.Forms.Button buttonScanFile; // Add a Button for scanning the file
+      private System.Windows.Forms.Button buttonExtractDictionary; // Rename buttonExportDictionary to buttonExtractDictionary
+      private System.Windows.Forms.Button buttonUploadDictionary; // Add a Button for uploading the dictionary
+      private System.Windows.Forms.Button buttonDictionarize; // Add a Button for dictionarizing
+      private System.Windows.Forms.Button buttonMerge; // Add a Button for merging
+      private System.Windows.Forms.DataGridView dataGridViewDictionary; // Change to DataGridView for displaying and editing the dictionary
+      private string selectedXmlFilePath;
+      private System.Windows.Forms.TextBox textBoxSearch; // Add a TextBox for searching
+      private System.Windows.Forms.Button buttonExportDictionary; // Add a Button for exporting the dictionary
+
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+      private void InitializeComponent()
+      {
+         buttonSelectXmlFile = new Button();
+         textBoxLogger = new TextBox();
+         listBoxXmlElements = new ListBox();
+         textBoxArrayObjectIdElementName = new TextBox();
+         textBoxArrayObjectNameElementName = new TextBox();
+         buttonScanFile = new Button();
+         buttonExtractDictionary = new Button();
+         buttonUploadDictionary = new Button();
+         buttonDictionarize = new Button();
+         buttonMerge = new Button();
+         dataGridViewDictionary = new DataGridView();
+         dataGridViewTextBoxColumn1 = new DataGridViewTextBoxColumn();
+         dataGridViewTextBoxColumn2 = new DataGridViewTextBoxColumn();
+         textBoxSearch = new TextBox();
+         buttonExportDictionary = new Button();
+         ((System.ComponentModel.ISupportInitialize)dataGridViewDictionary).BeginInit();
+         SuspendLayout();
+         // 
+         // buttonSelectXmlFile
+         // 
+         buttonSelectXmlFile.AllowDrop = true;
+         buttonSelectXmlFile.Location = new Point(12, 12);
+         buttonSelectXmlFile.Name = "buttonSelectXmlFile";
+         buttonSelectXmlFile.Size = new Size(150, 23);
+         buttonSelectXmlFile.TabIndex = 0;
+         buttonSelectXmlFile.Text = "Select .xml file";
+         buttonSelectXmlFile.UseVisualStyleBackColor = true;
+         buttonSelectXmlFile.Click += OnButtonSelectXmlFileClick;
+         buttonSelectXmlFile.DragDrop += OnButtonSelectXmlFileDragDrop;
+         buttonSelectXmlFile.DragEnter += OnButtonSelectXmlFileDragEnter;
+         // 
+         // textBoxLogger
+         // 
+         textBoxLogger.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+         textBoxLogger.Location = new Point(12, 50);
+         textBoxLogger.Multiline = true;
+         textBoxLogger.Name = "textBoxLogger";
+         textBoxLogger.ReadOnly = true;
+         textBoxLogger.Size = new Size(1033, 150);
+         textBoxLogger.TabIndex = 1;
+         // 
+         // listBoxXmlElements
+         // 
+         listBoxXmlElements.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+         listBoxXmlElements.ItemHeight = 15;
+         listBoxXmlElements.Location = new Point(12, 303);
+         listBoxXmlElements.Name = "listBoxXmlElements";
+         listBoxXmlElements.Size = new Size(388, 619);
+         listBoxXmlElements.TabIndex = 2;
+         // 
+         // textBoxArrayObjectIdElementName
+         // 
+         textBoxArrayObjectIdElementName.Location = new Point(12, 235);
+         textBoxArrayObjectIdElementName.Name = "textBoxArrayObjectIdElementName";
+         textBoxArrayObjectIdElementName.PlaceholderText = "Array object ID element name";
+         textBoxArrayObjectIdElementName.Size = new Size(200, 23);
+         textBoxArrayObjectIdElementName.TabIndex = 3;
+         // 
+         // textBoxArrayObjectNameElementName
+         // 
+         textBoxArrayObjectNameElementName.Location = new Point(12, 264);
+         textBoxArrayObjectNameElementName.Name = "textBoxArrayObjectNameElementName";
+         textBoxArrayObjectNameElementName.PlaceholderText = "Array object Name element name";
+         textBoxArrayObjectNameElementName.Size = new Size(200, 23);
+         textBoxArrayObjectNameElementName.TabIndex = 4;
+         // ...existing code...
+         textBoxArrayObjectNameElementName.Text = "Name"; // Set default value to "Name"
+         // 
+         // buttonScanFile
+         // 
+         buttonScanFile.Location = new Point(12, 206);
+         buttonScanFile.Name = "buttonScanFile";
+         buttonScanFile.Size = new Size(75, 23);
+         buttonScanFile.TabIndex = 5;
+         buttonScanFile.Text = "Scan File";
+         buttonScanFile.UseVisualStyleBackColor = true;
+         buttonScanFile.Click += OnButtonScanFileClick;
+         // 
+         // buttonExtractDictionary
+         // 
+         buttonExtractDictionary.Location = new Point(93, 206);
+         buttonExtractDictionary.Name = "buttonExtractDictionary";
+         buttonExtractDictionary.Size = new Size(150, 23);
+         buttonExtractDictionary.TabIndex = 6;
+         buttonExtractDictionary.Text = "Extract Dictionary";
+         buttonExtractDictionary.UseVisualStyleBackColor = true;
+         buttonExtractDictionary.Click += OnButtonExtractDictionaryClick;
+         // 
+         // buttonUploadDictionary
+         // 
+         buttonUploadDictionary.Location = new Point(250, 206);
+         buttonUploadDictionary.Name = "buttonUploadDictionary";
+         buttonUploadDictionary.Size = new Size(150, 23);
+         buttonUploadDictionary.TabIndex = 7;
+         buttonUploadDictionary.Text = "Upload Dictionary";
+         buttonUploadDictionary.UseVisualStyleBackColor = true;
+         buttonUploadDictionary.Click += OnButtonUploadDictionaryClick;
+         // 
+         // buttonDictionarize
+         // 
+         buttonDictionarize.Location = new Point(250, 264);
+         buttonDictionarize.Name = "buttonDictionarize";
+         buttonDictionarize.Size = new Size(150, 23);
+         buttonDictionarize.TabIndex = 11;
+         buttonDictionarize.Text = "Dictionarize";
+         buttonDictionarize.UseVisualStyleBackColor = true;
+         buttonDictionarize.Click += OnButtonDictionarizeClick;
+         // 
+         // buttonMerge
+         // 
+         buttonMerge.Location = new Point(718, 213);
+         buttonMerge.Name = "buttonMerge";
+         buttonMerge.Size = new Size(75, 23);
+         buttonMerge.TabIndex = 12;
+         buttonMerge.Text = "Merge";
+         buttonMerge.UseVisualStyleBackColor = true;
+         buttonMerge.Click += OnButtonMergeClick;
+         // 
+         // dataGridViewDictionary
+         // 
+         dataGridViewDictionary.AllowUserToAddRows = false;
+         dataGridViewDictionary.AllowUserToDeleteRows = false;
+         dataGridViewDictionary.AllowUserToResizeColumns = false;
+         dataGridViewDictionary.AllowUserToResizeRows = false;
+         dataGridViewDictionary.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+         dataGridViewDictionary.CausesValidation = false;
+         dataGridViewDictionary.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+         dataGridViewDictionary.Columns.AddRange(new DataGridViewColumn[] { dataGridViewTextBoxColumn1, dataGridViewTextBoxColumn2 });
+         dataGridViewDictionary.Location = new Point(406, 243);
+         dataGridViewDictionary.MultiSelect = false;
+         dataGridViewDictionary.Name = "dataGridViewDictionary";
+         dataGridViewDictionary.SelectionMode = DataGridViewSelectionMode.CellSelect;
+         dataGridViewDictionary.Size = new Size(639, 686);
+         dataGridViewDictionary.TabIndex = 9;
+         dataGridViewDictionary.CellContentClick += dataGridViewDictionary_CellContentClick_1;
+         // 
+         // dataGridViewTextBoxColumn1
+         // 
+         dataGridViewTextBoxColumn1.HeaderText = "ID";
+         dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+         dataGridViewTextBoxColumn1.ReadOnly = true;
+         dataGridViewTextBoxColumn1.Width = 50;
+         // 
+         // dataGridViewTextBoxColumn2
+         // 
+         dataGridViewTextBoxColumn2.HeaderText = "Name";
+         dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+         dataGridViewTextBoxColumn2.Width = 500;
+         // 
+         // textBoxSearch
+         // 
+         textBoxSearch.Location = new Point(406, 214);
+         textBoxSearch.Name = "textBoxSearch";
+         textBoxSearch.PlaceholderText = "Search";
+         textBoxSearch.Size = new Size(306, 23);
+         textBoxSearch.TabIndex = 9;
+         textBoxSearch.TextChanged += OnTextBoxSearchTextChanged;
+         // 
+         // buttonExportDictionary
+         // 
+         buttonExportDictionary.Location = new Point(250, 235);
+         buttonExportDictionary.Name = "buttonExportDictionary";
+         buttonExportDictionary.Size = new Size(150, 23);
+         buttonExportDictionary.TabIndex = 13;
+         buttonExportDictionary.Text = "Export Dictionary";
+         buttonExportDictionary.UseVisualStyleBackColor = true;
+         buttonExportDictionary.Click += OnButtonExportDictionaryClick;
+         // 
+         // Form1
+         // 
+         AutoScaleDimensions = new SizeF(7F, 15F);
+         AutoScaleMode = AutoScaleMode.Font;
+         ClientSize = new Size(1057, 944);
+         Controls.Add(buttonMerge);
+         Controls.Add(buttonDictionarize);
+         Controls.Add(buttonUploadDictionary);
+         Controls.Add(buttonExtractDictionary);
+         Controls.Add(buttonScanFile);
+         Controls.Add(textBoxArrayObjectNameElementName);
+         Controls.Add(textBoxArrayObjectIdElementName);
+         Controls.Add(listBoxXmlElements);
+         Controls.Add(textBoxLogger);
+         Controls.Add(buttonSelectXmlFile);
+         Controls.Add(dataGridViewDictionary);
+         Controls.Add(textBoxSearch);
+         Controls.Add(buttonExportDictionary);
+         Name = "Form1";
+         Text = "Form1";
+         ((System.ComponentModel.ISupportInitialize)dataGridViewDictionary).EndInit();
+         ResumeLayout(false);
+         PerformLayout();
+      }
+
+      private void OnButtonSelectXmlFileClick(object sender, EventArgs e)
+      {
+         SelectXmlFile();
+      }
+
+      private void OnButtonSelectXmlFileDragEnter(object sender, DragEventArgs e)
+      {
+         HandleDragEnter(e);
+      }
+
+      private void OnButtonSelectXmlFileDragDrop(object sender, DragEventArgs e)
+      {
+         HandleDragDrop(e);
+      }
+
+      private void OnButtonScanFileClick(object sender, EventArgs e)
+      {
+         ScanFile();
+      }
+
+      private void OnButtonExtractDictionaryClick(object sender, EventArgs e)
+      {
+         ExtractDictionary();
+      }
+
+      private void OnButtonUploadDictionaryClick(object sender, EventArgs e)
+      {
+         UploadDictionary();
+      }
+
+      private void OnTextBoxSearchByIdTextChanged(object sender, EventArgs e)
+      {
+         FilterDictionary();
+      }
+
+      private void OnTextBoxSearchByStringTextChanged(object sender, EventArgs e)
+      {
+         FilterDictionary();
+      }
+
+      private void OnButtonDictionarizeClick(object sender, EventArgs e)
+      {
+         Dictionarize();
+      }
+
+      private void OnButtonMergeClick(object sender, EventArgs e)
+      {
+         MergeDictionaryWithXml();
+      }
+
+      private void OnButtonExportDictionaryClick(object sender, EventArgs e)
+      {
+         ExportDictionary();
+      }
+      private DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+      private DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+   }
+}
---
+++ Form1.cs
@@ -0,0 +1,412 @@
+using System;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+using System.Linq;
+
+namespace DW2_xml_name_editor
+{
+   public partial class Form1 : Form
+   {
+      public Form1()
+      {
+         InitializeComponent();
+      }
+
+      private void SelectXmlFile()
+      {
+         using (OpenFileDialog openFileDialog = new OpenFileDialog())
+         {
+            openFileDialog.Filter = "XML files (*.xml)|*.xml|All files (*.*)|*.*";
+            openFileDialog.Title = "Select an XML file";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+               selectedXmlFilePath = openFileDialog.FileName;
+               LogMessage($"File selected: {selectedXmlFilePath}");
+
+               try
+               {
+                  XmlDocument xmlDoc = new XmlDocument();
+                  xmlDoc.Load(selectedXmlFilePath);
+                  XmlNode root = xmlDoc.DocumentElement;
+
+                  if (root != null)
+                  {
+                     string defaultIdElementName = FindIdElementName(root, 3);
+                     if (!string.IsNullOrEmpty(defaultIdElementName))
+                     {
+                        textBoxArrayObjectIdElementName.Text = defaultIdElementName;
+                     }
+                  }
+               }
+               catch (Exception ex)
+               {
+                  LogMessage($"Error loading XML file: {ex.Message}");
+               }
+            }
+         }
+      }
+
+      private string FindIdElementName(XmlNode node, int depth)
+      {
+         if (depth == 0 || node == null)
+         {
+            return null;
+         }
+
+         foreach (XmlNode childNode in node.ChildNodes)
+         {
+            if (childNode.Name.Contains("Id", StringComparison.OrdinalIgnoreCase))
+            {
+               return childNode.Name;
+            }
+
+            string result = FindIdElementName(childNode, depth - 1);
+            if (!string.IsNullOrEmpty(result))
+            {
+               return result;
+            }
+         }
+
+         return null;
+      }
+
+      private void HandleDragEnter(DragEventArgs e)
+      {
+         if (e.Data.GetDataPresent(DataFormats.FileDrop))
+         {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files.Length > 0 && Path.GetExtension(files[0]).ToLower() == ".xml")
+            {
+               e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+               e.Effect = DragDropEffects.None;
+            }
+         }
+      }
+
+      private void HandleDragDrop(DragEventArgs e)
+      {
+         if (e.Data.GetDataPresent(DataFormats.FileDrop))
+         {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files.Length > 0 && Path.GetExtension(files[0]).ToLower() == ".xml")
+            {
+               selectedXmlFilePath = files[0];
+               // Handle the selected file path as needed
+               LogMessage($"File selected: {selectedXmlFilePath}");
+               // PopulateListBoxWithXmlElements(selectedXmlFilePath); // Commented out for now
+            }
+         }
+      }
+
+      private void ScanFile()
+      {
+         string idElementName = textBoxArrayObjectIdElementName.Text;
+         string nameElementName = textBoxArrayObjectNameElementName.Text;
+
+         if (string.IsNullOrEmpty(idElementName) || string.IsNullOrEmpty(nameElementName))
+         {
+            LogMessage("Please enter both element names.");
+            return;
+         }
+
+         if (string.IsNullOrEmpty(selectedXmlFilePath))
+         {
+            LogMessage("No XML file selected.");
+            return;
+         }
+
+         PopulateListBoxWithXmlElements(selectedXmlFilePath, idElementName, nameElementName);
+      }
+
+      private void PopulateListBoxWithXmlElements(string filePath, string idElementName, string nameElementName)
+      {
+         try
+         {
+            LogMessage("Attempting to load XML file.");
+            listBoxXmlElements.Items.Clear();
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(filePath);
+            LogMessage("XML file loaded successfully.");
+
+            XmlNode root = xmlDoc.DocumentElement;
+            if (root == null)
+            {
+               LogMessage("Invalid XML structure: Root element not found.");
+               return;
+            }
+
+            foreach (XmlNode childNode in root.ChildNodes)
+            {
+               XmlNode idNode = childNode.SelectSingleNode(idElementName);
+               XmlNode nameNode = childNode.SelectSingleNode(nameElementName);
+
+               if (idNode != null && nameNode != null)
+               {
+                  string listItem = $"{idNode.InnerText};{nameNode.InnerText}";
+                  listBoxXmlElements.Items.Add(listItem);
+                  LogMessage($"Element added to ListBox: {listItem}");
+               }
+            }
+            LogMessage("All elements processed successfully.");
+         }
+         catch (Exception ex)
+         {
+            LogMessage($"Error processing XML file: {ex.Message}");
+         }
+      }
+
+      // ...existing code...
+      private void ExportDictionary()
+      {
+         using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+         {
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog.Title = "Save Dictionary";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+               string filePath = saveFileDialog.FileName;
+               try
+               {
+                  using (StreamWriter writer = new StreamWriter(filePath))
+                  {
+                     foreach (DataGridViewRow row in dataGridViewDictionary.Rows)
+                     {
+                        if (row.Cells["dataGridViewTextBoxColumn1"].Value != null && row.Cells["dataGridViewTextBoxColumn2"].Value != null)
+                        {
+                           string line = $"{row.Cells["dataGridViewTextBoxColumn1"].Value};{row.Cells["dataGridViewTextBoxColumn2"].Value}";
+                           writer.WriteLine(line);
+                        }
+                     }
+                  }
+                  LogMessage($"Dictionary exported to: {filePath}");
+               }
+               catch (Exception ex)
+               {
+                  LogMessage($"Error exporting dictionary: {ex.Message}");
+               }
+            }
+         }
+      }
+
+      private void UploadDictionary()
+      {
+         using (OpenFileDialog openFileDialog = new OpenFileDialog())
+         {
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.Title = "Upload Dictionary";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+               string filePath = openFileDialog.FileName;
+               try
+               {
+                  dataGridViewDictionary.Rows.Clear();
+                  using (StreamReader reader = new StreamReader(filePath))
+                  {
+                     string line;
+                     while ((line = reader.ReadLine()) != null)
+                     {
+                        var parts = line.Split(';');
+                        if (parts.Length == 2)
+                        {
+                           dataGridViewDictionary.Rows.Add(parts[0], parts[1]);
+                        }
+                     }
+                  }
+                  LogMessage($"Dictionary uploaded from: {filePath}");
+               }
+               catch (Exception ex)
+               {
+                  LogMessage($"Error uploading dictionary: {ex.Message}");
+               }
+            }
+         }
+      }
+
+      // ...existing code...
+      private void FilterDictionary()
+      {
+         try
+         {
+            string searchText = textBoxSearch.Text;
+            bool isNumber = int.TryParse(searchText, out int searchId);
+
+            var rows = dataGridViewDictionary.Rows.Cast<DataGridViewRow>().ToList();
+            List<DataGridViewRow> exactMatches;
+            List<DataGridViewRow> partialMatches;
+
+            if (isNumber)
+            {
+               // If the search text is a number, match against IDs
+               exactMatches = rows.Where(row => row.Cells[0].Value.ToString() == searchId.ToString()).ToList();
+               partialMatches = rows.Except(exactMatches).ToList();
+            }
+            else
+            {
+               // If the search text is not a number, find the closest matching strings
+               exactMatches = rows.Where(row => row.Cells[1].Value.ToString().Contains(searchText, StringComparison.OrdinalIgnoreCase)).ToList();
+               partialMatches = rows.Except(exactMatches)
+                                   .OrderBy(row => LevenshteinDistance(row.Cells[1].Value.ToString(), searchText))
+                                   .ToList();
+            }
+
+            dataGridViewDictionary.Rows.Clear();
+            foreach (var row in exactMatches.Concat(partialMatches))
+            {
+               dataGridViewDictionary.Rows.Add(row.Cells[0].Value, row.Cells[1].Value);
+            }
+         }
+         catch (Exception ex)
+         {
+            LogMessage($"Error filtering dictionary: {ex.Message}");
+         }
+      }
+
+      // ...existing code...
+
+      private int LevenshteinDistance(string source, string target)
+      {
+         if (string.IsNullOrEmpty(source)) return target.Length;
+         if (string.IsNullOrEmpty(target)) return source.Length;
+
+         int[,] distance = new int[source.Length + 1, target.Length + 1];
+
+         for (int i = 0; i <= source.Length; distance[i, 0] = i++) { }
+         for (int j = 0; j <= target.Length; distance[0, j] = j++) { }
+
+         for (int i = 1; i <= source.Length; i++)
+         {
+            for (int j = 1; j <= target.Length; j++)
+            {
+               int cost = (target[j - 1] == source[i - 1]) ? 0 : 1;
+               distance[i, j] = Math.Min(
+                   Math.Min(distance[i - 1, j] + 1, distance[i, j - 1] + 1),
+                   distance[i - 1, j - 1] + cost);
+            }
+         }
+
+         return distance[source.Length, target.Length];
+      }
+
+      private void OnTextBoxSearchTextChanged(object sender, EventArgs e)
+      {
+         FilterDictionary();
+      }
+
+      private void Dictionarize()
+      {
+         dataGridViewDictionary.Rows.Clear();
+         foreach (var item in listBoxXmlElements.Items)
+         {
+            var parts = item.ToString().Split(';');
+            if (parts.Length == 2)
+            {
+               dataGridViewDictionary.Rows.Add(parts[0], parts[1]);
+            }
+         }
+      }
+
+      private void MergeDictionaryWithXml()
+      {
+         try
+         {
+            if (string.IsNullOrEmpty(selectedXmlFilePath))
+            {
+               LogMessage("No XML file selected.");
+               return;
+            }
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(selectedXmlFilePath);
+            XmlNode root = xmlDoc.DocumentElement;
+            if (root == null)
+            {
+               LogMessage("Invalid XML structure: Root element not found.");
+               return;
+            }
+
+            var dictionary = new Dictionary<string, string>();
+            foreach (DataGridViewRow row in dataGridViewDictionary.Rows)
+            {
+               if (row.Cells["dataGridViewTextBoxColumn1"].Value != null && row.Cells["dataGridViewTextBoxColumn2"].Value != null)
+               {
+                  dictionary[row.Cells["dataGridViewTextBoxColumn1"].Value.ToString()] = row.Cells["dataGridViewTextBoxColumn2"].Value.ToString();
+               }
+            }
+
+            foreach (XmlNode childNode in root.ChildNodes)
+            {
+               XmlNode idNode = childNode.SelectSingleNode(textBoxArrayObjectIdElementName.Text);
+               XmlNode nameNode = childNode.SelectSingleNode(textBoxArrayObjectNameElementName.Text);
+
+               if (idNode != null && nameNode != null && dictionary.ContainsKey(idNode.InnerText))
+               {
+                  nameNode.InnerText = dictionary[idNode.InnerText];
+               }
+            }
+
+            string directory = Path.GetDirectoryName(selectedXmlFilePath);
+            string filenameWithoutExtension = Path.GetFileNameWithoutExtension(selectedXmlFilePath);
+            string extension = Path.GetExtension(selectedXmlFilePath);
+            string newFilePath = Path.Combine(directory, $"{filenameWithoutExtension}_Mod{extension}");
+
+            xmlDoc.Save(newFilePath);
+            LogMessage($"Merged XML file saved to: {newFilePath}");
+         }
+         catch (Exception ex)
+         {
+            LogMessage($"Error merging dictionary with XML: {ex.Message}");
+         }
+      }
+
+      private void ExtractDictionary()
+      {
+         using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+         {
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog.Title = "Save Dictionary";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+               string filePath = saveFileDialog.FileName;
+               try
+               {
+                  using (StreamWriter writer = new StreamWriter(filePath))
+                  {
+                     foreach (var item in listBoxXmlElements.Items)
+                     {
+                        writer.WriteLine(item.ToString());
+                     }
+                  }
+                  LogMessage($"Dictionary exported to: {filePath}");
+               }
+               catch (Exception ex)
+               {
+                  LogMessage($"Error exporting dictionary: {ex.Message}");
+               }
+            }
+         }
+      }
+
+      private void LogMessage(string message)
+      {
+         textBoxLogger.AppendText($"{DateTime.Now}: {message}{Environment.NewLine}");
+      }
+
+      private void dataGridViewDictionary_CellContentClick(object sender, DataGridViewCellEventArgs e)
+      {
+
+      }
+
+      private void dataGridViewDictionary_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+      {
+
+      }
+   }
+}
